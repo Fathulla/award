8a9d75ce753216d4759b3869b39067dda5adc78d
Fathulla Rashidov <fathullarasidov239@gmail.com>
2024-11-30 21:11:37 +0500

    minor changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
           <ImageClipBox clipClass={"sword-man-clip-path md:scale-125"} src="img/swordman.webp"/>
         </div>
         <div className="flex flex-col items-center text-center">
-          <p className="font-general text-[10px] uppercase">Join Zentry</p>
+          <p className="font-general text-[10px] uppercase">Join Zentry And Play</p>
           <p className={"special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]"}>
             Let's b<b>u</b>ild the <br/> new era of <br/> g<b>a</b>ming t<b>o</b>gether
           </p>
